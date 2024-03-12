de9f568d104ca8fbbb83a16bd5c16733dd0d17e3
Lokesh Babchode <108623364+loki7349@users.noreply.github.com>
2024-03-12 16:15:01 +0000

    added the landing page

--- job_finder/src/pages/Home.jsx
+++ job_finder/src/pages/Home.jsx
@@ -1,8 +1,10 @@
+import Hero from "../Components/Hero"
 import Navbar from "../Components/Navbar"
 function Home(){
     return(
         <div>
             <Navbar />
+            <Hero />
         </div>
     )
         
